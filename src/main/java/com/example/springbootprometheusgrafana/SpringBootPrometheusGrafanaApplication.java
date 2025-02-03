40e7c3713ab80759e318f06cf1a389d365d6f61d
Felipe Burin <fburin@gmail.com>
2025-02-03 16:36:42 -0300

    first commit

---
+++ src/main/java/com/example/springbootprometheusgrafana/SpringBootPrometheusGrafanaApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootprometheusgrafana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPrometheusGrafanaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootPrometheusGrafanaApplication.class, args);
+	}
+
+}
